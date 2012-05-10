b36e71c3a807644e5c29ab7d520918ea897cb097
Daniel Spillere Andrade <daniel@danielandrade.net>
2012-05-10 14:49:35 +0200

    Organizing

---
+++ Code/Code.ino
@@ -0,0 +1,14 @@
+/*
+ Open Hardware Mood Lamp  
+ By: Daniel Spillere Andrade - www.DanielAndrade.net - daniel@danielandrade.net - https://github.com/dansku
+ https://github.com/dansku/Open-Hardware-Mood-Lamp
+ */
+
+void setup() {                
+   
+}
+
+void loop() {
+   //The Code will come soon! :)
+}
+
